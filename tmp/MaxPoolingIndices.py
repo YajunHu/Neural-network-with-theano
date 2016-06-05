ad3eb9a87861783c589e19841ed60912d8f5bd33
nagichan <yajhu@hotmail.com>
2016-06-05 23:09:39 +0800

    commit

--- tmp/MaxPoolingIndices.py
+++ tmp/MaxPoolingIndices.py
@@ -6,7 +6,7 @@ from theano.tensor.signal.pool import max_pool_2d_same_size
 
 
 input = T.tensor4()
-pool_out = max_pool_2d(input, ds=(2,2))
+pool_out = max_pool_2d(input, ds=(2,2),ignore_border=True)
 method = 1
 
 if method==1:	
@@ -19,7 +19,7 @@ elif method==3:
 	pool_same_size = max_pool_2d_same_size(input,(2,2))
 	#indices = pool_same_size>0 #get 0/1 indicating non/argmax
 	indices = pool_same_size.nonzero() #get an array of the cordinates of argmax	
-	unpool = T.set_subtensor(T.zeros_like(input)[pool_same_size.nonzero()],T.flatten(pool_out))
+	unpool = T.set_subtensor(T.zeros_like(input)[indices],T.flatten(pool_out))
 
 f_index = theano.function([input],indices)
 f_unpool = theano.function([input],unpool)
