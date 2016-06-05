e528d05a1f3539955c06168d3f3b7886e37d6bc6
YajunHu <hyj15475@mail.ustc.edu.cn>
2016-06-05 17:05:00 +0800

    successful unpooling with argmax

--- tmp/MaxPoolingIndices.py
+++ tmp/MaxPoolingIndices.py
@@ -1,7 +1,8 @@
 import theano
 import numpy as np
 import theano.tensor as T
-from theano.tensor.signal.downsample import max_pool_2d,max_pool_2d_same_size
+from theano.tensor.signal.downsample import max_pool_2d
+from theano.tensor.signal.pool import max_pool_2d_same_size
 
 
 input = T.tensor4()
@@ -15,10 +16,14 @@ elif method==2:
 	indices,pool_out = max_pool_2d(input, ds=(2,2)) #modifiy the code of max_pool_2d, to be completed
 	
 elif method==3:
-	pool_same_size = max_pool_2d_same_size(input,ds=(2,2))
-	indices = pool_same_size>0
+	pool_same_size = max_pool_2d_same_size(input,(2,2))
+	#indices = pool_same_size>0 #get 0/1 indicating non/argmax
+	indices = pool_same_size.nonzero() #get an array of the cordinates of argmax	
+	unpool = T.set_subtensor(T.zeros_like(input)[pool_same_size.nonzero()],T.flatten(pool_out))
 
-f = theano.function([input],indices)
+f_index = theano.function([input],indices)
+f_unpool = theano.function([input],unpool)
 a = np.arange(16).reshape(1, 1, 4, 4)
+
 print f(a)	
 
